690b3a2f4f570770b1e6d60bf2435056f127d50d
AhamedSabid <68531236+AhamedSabid@users.noreply.github.com>
2022-09-30 19:20:09 +0530

    Update ParallelRun.java

--- src/test/java/KarateAutomationDemo/KarateAutomationDemo/ParallelRun.java
+++ src/test/java/KarateAutomationDemo/KarateAutomationDemo/ParallelRun.java
@@ -1,20 +1,30 @@
 package KarateAutomationDemo.KarateAutomationDemo;
 
 
+import org.junit.runner.RunWith;
+import org.testng.annotations.DataProvider;
+
+import com.intuit.karate.junit4.Karate;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
+@RunWith(Karate.class)
 @CucumberOptions(
-		plugin = {"html:Reports/cucumber-html-report.html"}, 
-		monochrome = true,
-		features = { "src/test/java/KarateAutomationDemo/KarateAutomationDemo" }
-                 )
+/*
+ * plugin = {"html:Reports/cucumber-html-report.html"}, monochrome = true,
+ * features = { "src/test/java/KarateAutomationDemo/KarateAutomationDemo" }
+ */
+)
 
 public class ParallelRun {
-	/*extends AbstractTestNGCucumberTests {
-	@Override
-	@DataProvider(parallel = true)
-	public Object[][] scenarios() {
-		return super.scenarios();
-	}
-	*/
+	//extends AbstractTestNGCucumberTests {
+	
+	/*
+	 * @Override
+	 * 
+	 * @DataProvider(parallel = true) public Object[][] scenarios() { return
+	 * super.scenarios(); }
+	 */
+	 
 }
