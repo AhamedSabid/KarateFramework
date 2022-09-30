3f53398ed4403be54fac0a0599f2ad496ce20ded
AhamedSabid <68531236+AhamedSabid@users.noreply.github.com>
2022-09-30 19:18:27 +0530

    Update ParallelRun.java

--- src/test/java/KarateAutomationDemo/KarateAutomationDemo/ParallelRun.java
+++ src/test/java/KarateAutomationDemo/KarateAutomationDemo/ParallelRun.java
@@ -1,9 +1,6 @@
 package KarateAutomationDemo.KarateAutomationDemo;
 
 
-import org.testng.annotations.DataProvider;
-
-import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
